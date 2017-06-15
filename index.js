8fa91d4dd6434e951ec8628f5a16e89452d93521
bredele <olivier.wietrich@gmail.com>
2017-06-15 15:53:07 -0600

    Add example folder

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+/**
+ * Dependencies.
+ */
+
+const fs = require('fs')
+
 
 /**
- * This is a simple description.
+ * Create HTTP methods middleware from folder structure.
  *
+ * @param {String} folder
+ * @param {HttpIncomingMessage} request
+ * @return {Stream}
  * @api public
  */
 
-module.exports = function () {
-  // do something
+module.exports = function (folder, request) {
+  routes(folder)
 }
 
-  
+/**
+ * Give routes from folder structure
+ *
+ *
+ * @return {Object}
+ * @api private
+ */
+
+function routes (folder) {
+  fs.readdir(__dirname + '/' + folder, (err, files) => {
+    if (err) return
+    console.log(files)
+  })
+}
