1983bc127c8826a641098bf69459f8b3e3349ded
bredele <olivier.wietrich@gmail.com>
2017-07-13 10:59:07 -0600

    Call folder-endpoint from request

--- test/beautiful-manner.js
+++ test/beautiful-manner.js
@@ -5,10 +5,21 @@
 
 const test = require('tape')
 const endpoint = require('..')
+const server = require('server-test')
+const concat = require('concat-stream')
 
 
 test('should create endpoint from folder path', assert => {
   assert.plan(1)
   const api = endpoint(__dirname + '/fixtures')
   assert.equal(typeof api, 'function')
+})
+
+
+test('should call endpoint method on request', assert => {
+  assert.plan(1)
+  const api = endpoint(__dirname + '/fixtures')
+  server((req, res) => {
+    api(req, res).pipe(concat(data => assert.equal(data, 'hello world')))
+  })
 })
