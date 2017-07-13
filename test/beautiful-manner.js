3252ab30a59c85e66b6b009f417267fcea1d4e54
bredele <olivier.wietrich@gmail.com>
2017-07-13 11:01:56 -0600

    Recursively create endpoints from folder

--- test/beautiful-manner.js
+++ test/beautiful-manner.js
@@ -11,15 +11,29 @@ const concat = require('concat-stream')
 
 test('should create endpoint from folder path', assert => {
   assert.plan(1)
-  const api = endpoint(__dirname + '/fixtures')
+  const api = endpoint(__dirname + '/hello')
   assert.equal(typeof api, 'function')
 })
 
 
 test('should call endpoint method on request', assert => {
   assert.plan(1)
-  const api = endpoint(__dirname + '/fixtures')
+  const api = endpoint(__dirname + '/hello')
   server((req, res) => {
+    api(req, res).pipe(concat(data => assert.equal(data, 'hello world')))
+  })
+})
+
+
+test('should recursively create endpoints from folder', assert => {
+  assert.plan(2)
+  const api = endpoint(__dirname)
+  server((req, res) => {
+    req.url = '/foo'
+    api(req, res).pipe(concat(data => assert.equal(data, 'hello bar')))
+  })
+  server((req, res) => {
+    req.url = '/hello'
     api(req, res).pipe(concat(data => assert.equal(data, 'hello world')))
   })
 })
--- test/fixtures/index.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = {
-  get() {
-    return 'hello world'
-  }
-}
